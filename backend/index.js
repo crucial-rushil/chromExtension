39ffe2789c64cb1575f9f148f164220bb91e0016
Rushil Shah <rushilshah2016@gmail.com>
2024-07-01 01:15:19 -0700

    Install packages and establish a connection with MongoDB.

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,42 @@
 import express from "express"
 import mongoose from "mongoose";
-import workoutRoutes from './routes/workouts.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-//express App
+import dotenv from 'dotenv';
+
+//Load and Define Sensitive Information from Environment Variables
+dotenv.config();
+
+//make an express App
 const app = express()
 
-// middleware
+//specify port number
+const port = process.env.PORT || 4000; 
+
+//NOTE: we should eventually keep only one of the formats to PARSE requests and send all requests in that format
+//automatically PARSE all requests sent in JSON Format
 app.use(express.json())
+
+//automatically PARSE all requests sent in url encoded Format
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//utilize third paty package to parse requests
+app.use(bodyParser.json());
+
+//MASSIVE SECURITY VULERNABILITY (CHANGE LATER TO CHROME EXTENSION ID) OK FOR LOCAL DEV PURPOSES
+//currently anyone can send requests to our backend
 app.use(cors());
 
 app.use((req, res, next)=> {
     console.log(req.path, req.method)
-    next()
+    next() //pass control to next middleware function
 })
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Parse application/json
-app.use(bodyParser.json());
-
-//react to requests
-app.use('/api/workouts',workoutRoutes)
-
-//connect to mongoDB
-mongoose.connect("mongodb+srv://rushilshah2016:Pineapple424$@chromextensions.dgjlaip.mongodb.net/?retryWrites=true&w=majority&appName=chromExtensions")
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend.');
+});
+var connection_string = process.env.DB_CONNECTION
+mongoose.connect(connection_string)
     .then(()=> {
         //listen from requesets
         app.listen(4000, ()=>{
@@ -34,3 +46,8 @@ mongoose.connect("mongodb+srv://rushilshah2016:Pineapple424$@chromextensions.dgj
     .catch((error)=>{
         console.log(error)
     })
+
+// Start the server
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
