b5bb2567c963c663189b3529f11c4c84d89fcbad
Ava Shah <avajshah@gmail.com>
2024-07-01 22:06:53 -0700

    css text and title changes

--- house-review/src/App.js
+++ house-review/src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+// change title to include the user name
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome back Ava! 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
